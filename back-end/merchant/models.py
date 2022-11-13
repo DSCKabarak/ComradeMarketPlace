a7b563e58ed58f856cd182dc48e173c1bb40be63
Ek Chess <ekchesire2@gmail.com>
2022-11-13 19:57:06 +0300

    making migrations

--- back-end/merchant/models.py
+++ back-end/merchant/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-#from django_autoslug.fields import AutoSlugField
+from autoslug import AutoSlugField
 
 class Category(models.Model):
-
     category_name = models.CharField(max_length=256)
     sub_category = models.CharField(max_length=256)
-    #slug = AutoSlugField(unique_by='id', populate_form='category_name')
+    slug = AutoSlugField(unique_with='id', populate_from='category_name')
 
     def __str__(self):
         category = f'{self.category_name}, {self.sub_category}'
@@ -18,8 +17,19 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(Category, verbose_name=("categories"), on_delete=models.CASCADE)
     name = models.CharField(max_length=256)
+    price = models.IntegerField()
+    in_stock = models.BooleanField(default=False)
+    tag = models.CharField(max_length=256)
+    slug = AutoSlugField(unique_with='id', populate_from='name')
+    brand = models.CharField(max_length=256)
+    key_features = models.TextField()
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+
     class Meta:
         db_table = 'products'
