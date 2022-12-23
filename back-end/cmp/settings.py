cfe6a63d1791089495d9e5c171a69ab2c67540ed
Michael Wekesa <wekesa360@outlook.com>
2022-12-23 17:52:44 +0300

    update: set email backend environment variables

--- back-end/cmp/settings.py
+++ back-end/cmp/settings.py
@@ -116,6 +116,14 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Email Backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
