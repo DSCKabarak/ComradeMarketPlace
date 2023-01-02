ad805d76e6081a3fe62dea5d7c60cebfb4484380
Michael Wekesa <wekesa360@outlook.com>
2023-01-02 13:48:43 +0300

    update: add helper functions

---
+++ back-end/accounts/utils.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework import serializers
+
+
+def get_and_authenticate_user(email, password):
+    user = authenticate(username=email, password=password)
+    if user is None:
+        raise serializers.ValidationError("Invalid username/password. Please try again!")
+    return user
+
+def create_user_account(email, password, first_name='', last_name='',
+                        **extra_fields):
+    user = get_user_model().objects.create_user(
+        email=email, password=password, first_name=first_name,
+        last_name=last_name, **extra_fields)
+    return user
