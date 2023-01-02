192fdf373bff5c0296fd4e8f32ddf7e7133c8dfb
Michael Wekesa <wekesa360@outlook.com>
2023-01-02 13:48:23 +0300

    update: add separate users app

---
+++ back-end/accounts/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from phonenumber_field.phonenumber import PhoneNumber
+
+# Create your models here.
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    # Method to save user to the database
+    def save_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+
+        # Call this method for password hashing
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields['is_superuser'] = False
+        extra_fields['is_staff'] = False
+        return self.save_user(email, password, **extra_fields)
+
+    # Method called while creating a staff user
+    def create_staffuser(self, email, password, **extra_fields):
+        extra_fields['is_staff'] = True
+        extra_fields['is_superuser'] = False
+        
+        return self.save_user(email, password, **extra_fields) 
+
+    # Method called while calling creatsuperuser
+    def create_superuser(self, email, password, **extra_fields):
+
+        # Set is_superuser parameter to true
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('is_superuser should be True')
+        
+        extra_fields['is_staff'] = True
+
+        return self.save_user(email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    TYPE_CHOICES = (
+        ('', 'Select'),
+        ('merchant', 'merchant'),
+        ('buyer', 'buyer')
+    )
+    username = None
+    email = models.EmailField(('email address'), unique=True)
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    bio = models.TextField()
+    phone_number = models.CharField(max_length=10, default='07XXXXXXXX')
+    user_type = models.CharField(max_length=90, choices=TYPE_CHOICES)
+    avatar = models.FileField(upload_to='user/uploads/avatar/', null=True) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+
+    def __str__(self) -> str:
+        return self.email
+
+    def get_avatar_url(self) -> str: #image url
+        return self.avatar.url
+    
+    class Meta:
+        db_table = 'users'
