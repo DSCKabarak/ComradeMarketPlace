52073a5d9ace87a46d154f0700a450121217fe7f
SoftCysec <softengdedan@gmail.com>
2023-01-13 20:49:03 +0300

    Jazzmin added

--- back-end/cmp/settings.py
+++ back-end/cmp/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
