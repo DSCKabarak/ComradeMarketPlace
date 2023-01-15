9d513c0f40c3adbdbbc6a5b0aaa9cab2f4b06497
Michael Wekesa <wekesa360@outlook.com>
2023-01-15 22:38:25 +0300

    update: add urls

--- back-end/cmp_api/urls.py
+++ back-end/cmp_api/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import (
-    ProductListAPIView,
+    ProductViewSet,
     AuthViewSet,
+    CommentViewSet,
 )
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register('api/auth', AuthViewSet, basename='auth')
+router.register('api/products', ProductViewSet, basename='products')
+router.register('api/comments', CommentViewSet, basename='comments')
+
 urlpatterns = [
     path(r'', include(router.urls)),
-    path('products', ProductListAPIView.as_view(), name='products')
 ]
