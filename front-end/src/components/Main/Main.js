83cacf2d4000c654374e192e8367ee08167f25d6
Ivan Bowen <bowenivan16@gmail.com>
2023-01-29 15:32:13 +0300

    refactor routes

--- front-end/src/components/Main/Main.js
+++ front-end/src/components/Main/Main.js
@@ -2,31 +2,16 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Login from '../Login/Login';
 import Products from '../Products/Products';
-import PrivateRoute from './PrivateRoute';
 
 const Main = () => {
-   const [isAuthenticated, setLoggedin] = React.useState(false);
-
-   const login = () => {
-      setLoggedin(true);
-      console.log("You are authenticated")
-   };
-
-   const logout = () => {
-      setLoggedin(false);
-   };
 
    return (
       <main>
          <Routes>
-            <Route path="/" element={<Login login={login} />} />
+            <Route path="/login" element={<Login />} />
             <Route
-               path="/products"
-               element={
-                  <PrivateRoute isAuthenticated={isAuthenticated}>
-                     <Products />
-                  </PrivateRoute>
-               }
+               path="/"
+               element={<Products />}
             />
          </Routes>
       </main>
