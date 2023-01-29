5eee767c31439b10127adeac57dc8d35f4b2730b
Ivan Bowen <bowenivan16@gmail.com>
2023-01-29 15:36:17 +0300

    retrieve user's info frm browser's sessionStorage

--- front-end/src/components/Products/Products.js
+++ front-end/src/components/Products/Products.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Products = () => {
+  const userString = sessionStorage.getItem("user");
+  const user = JSON.parse(userString);
+
   return (
-    <div>Products</div>
+
+    <div onClick={() => {
+      sessionStorage.setItem("user", null);
+      window.location.reload()}}>Welcome to KabuShop {user ? `, ${user.first_name}` : <Link to="/login">Login</Link>}</div>
   )
 }
 
