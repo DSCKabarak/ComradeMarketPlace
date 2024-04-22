2cb70cf8508e0e60273f774067f26d64214e521c
Michael Wekesa <wekesa360@outlook.com>
2024-04-22 15:22:22 +0300

    Update Django settings for production environment

--- back-end/cmp/settings/production.py
+++ back-end/cmp/settings/production.py
@@ -3,14 +3,14 @@ from .base import *
 DEBUG = False
 
 DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': cmp_db_name,
-        'USER': cmp_db_user,
-        'PASSWORD': cmp_db_password,
-        'HOST': cmp_db_host,
-        'PORT': cmp_db_port,
-        }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": cmp_db_name,
+        "USER": cmp_db_user,
+        "PASSWORD": cmp_db_password,
+        "HOST": cmp_db_host,
+        "PORT": cmp_db_port,
+    }
 }
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
