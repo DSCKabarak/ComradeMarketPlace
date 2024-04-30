4401a69bd206216678eefabc0252606a15484b15
nafdev01 <naftaldev@gmail.com>
2024-04-30 14:45:09 +0300

    Update Celery configuration for dynamic Django settings module

--- back-end/cmp/celery.py
+++ back-end/cmp/celery.py
@@ -1,7 +1,10 @@
 import os
 from celery import Celery
+from dotenv import load_dotenv
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cmp.settings.development")
+load_dotenv()
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"cmp.settings.{os.getenv('SETTINGS')}")
 app = Celery("cmp")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
