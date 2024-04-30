fb3360d07ab24ccfe9cf659bdf4854208b2cc905
nafdev01 <naftaldev@gmail.com>
2024-04-30 12:43:35 +0300

    Add Celery configuration for background task processing

---
+++ back-end/cmp/__init__.py
@@ -0,0 +1,5 @@
+# django_celery/__init__.py
+
+from .celery import app as celery_app
+
+__all__ = ("celery_app",)
--- back-end/cmp/settings/development.py
+++ back-end/cmp/settings/development.py
@@ -12,3 +12,16 @@ DATABASES = {
         "PORT": cmp_db_port,
     }
 }
+
+
+# celery settings
+# CELERY_BROKER_URL = "amqp://localhost"
+# CELERY_RESULT_BACKEND = "amqp://localhost"
+
+# email settings
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS").lower() == "true"
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
