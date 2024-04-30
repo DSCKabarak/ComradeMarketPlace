a9b484606b4df1d4abd556540f5417e621d6326c
Michael Wekesa <wekesa360@outlook.com>
2024-04-30 19:05:31 +0300

    Update base.py with REST framework and JWT configurations

--- back-end/cmp/settings/base.py
+++ back-end/cmp/settings/base.py
@@ -110,11 +110,13 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "AUTH_HEADER_TYPES": ("Bearer",),
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATION_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
 JWT_AUTH_HEADER_PREFIX = "Bearer "
