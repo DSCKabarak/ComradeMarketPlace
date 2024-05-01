3650deb62a23ea7ef039c9c72e62ec2497b01db4
nafdev01 <naftaldev@gmail.com>
2024-05-01 07:32:38 +0300

    Update Celery logging configuration

--- back-end/cmp/celery.py
+++ back-end/cmp/celery.py
@@ -1,5 +1,8 @@
+import logging
 import os
+
 from celery import Celery
+from celery.signals import after_setup_task_logger
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,3 +11,14 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"cmp.settings.{os.getenv('SETTI
 app = Celery("cmp")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+def setup_logging(**kwargs):
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler = logging.FileHandler('log/celery.log')
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger('celery')
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+after_setup_task_logger.connect(setup_logging)
