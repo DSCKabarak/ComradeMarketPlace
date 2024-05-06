378412ba378c694b599b23ee8d71a8e6d67a0cfe
Michael Wekesa <wekesa360@outlook.com>
2024-05-06 16:24:15 +0300

    Refactor Category and Product models in marketplace/models.py

--- back-end/marketplace/models.py
+++ back-end/marketplace/models.py
@@ -1,9 +1,7 @@
 import json
 import uuid
-
-from autoslug import AutoSlugField
+from django.utils.text import slugify
 from django.db import models
-
 from accounts.models import CustomUser
 from notifications.notify import send_new_category_notification
 from django.db.models.signals import post_save
@@ -11,22 +9,25 @@ from django.dispatch import receiver
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=256)
+    category = models.CharField(max_length=256)
     sub_category = models.CharField(max_length=256)
-    slug = AutoSlugField(unique_with="id", populate_from="category_name")
+    slug = models.CharField(max_length=256, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        category = f"{self.category_name}, {self.sub_category}"
+        category = f"{self.category}, {self.sub_category}"
         return category
+    
+    def save(self, *args, **kwargs):
+        self.slug  = slugify(f"{self.category}-{self.sub_category}")
+        super().save(*args, **kwargs)
 
     def get_category_name(self):
-        return f"{self.category_name}, {self.sub_category}"
+        return f"{self.category}, {self.sub_category}"
 
     class Meta:
         db_table = "categories"
-
-    def get_absolute_url(self):
-        return self.name
 
 
 @receiver(post_save, sender=Category)
@@ -36,33 +37,36 @@ def create_category(sender, instance, created, **kwargs):
 
 
 class Product(models.Model):
-    merchant = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    merchant = models.ForeignKey(CustomUser, on_delete=models.NOTHING)
     category = models.ForeignKey(
-        Category, verbose_name=("categories"), on_delete=models.CASCADE
+        Category, verbose_name=("categories"), on_delete=models.NOTHING
     )
     product_name = models.CharField(max_length=256)
     price = models.IntegerField()
     in_stock = models.BooleanField(default=False)
     tag = models.CharField(max_length=256)
-    slug = AutoSlugField(unique_with="id", populate_from="product_name")
+    slug = models.CharField(max_length=256, blank=True, null=True)
     brand = models.CharField(max_length=256)
     key_features = models.TextField()
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def get_absolute_url(self):
-        return self.name
-
     def __str__(self):
         return self.product_name
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(f"{self.id}-{self.product_name}")
+        super().save(*args, **kwargs)
+
+    
 
     class Meta:
         db_table = "products"
 
 
 class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.NOTHING)
     image = models.ImageField(upload_to="uploads/product/images/")
     description = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -72,7 +76,7 @@ class ProductImage(models.Model):
         return self.image.url
 
     def __str__(self):
-        return self.name
+        return self.product.product_name
 
     class Meta:
         db_table = "images"
